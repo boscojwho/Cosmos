cffcf56e7f716ab8db2bf6fe972e05e310995eae
Bosco Ho <boscojwho@gmail.com>
2023-08-24 03:31:51 -0700

    Add API request logic.

---
+++ Cosmos/Lemmy/APIRequest.swift
@@ -0,0 +1,71 @@
+//
+//  APIRequest.swift
+//  Cosmos
+//
+//  Created by Bosco Ho on 2023-08-24.
+//
+
+//  Adapted from:
+//
+//  APIRequest.swift
+//  Mlem
+//
+//  Created by Nicholas Lawson on 06/06/2023.
+//
+
+import Foundation
+
+// MARK: - APIRequest
+
+protocol APIRequest {
+    associatedtype Response: Decodable
+    
+    var path: String { get }
+    var instanceURL: URL { get }
+    var endpoint: URL { get }
+    var headers: [String: String] { get }
+}
+
+extension APIRequest {
+    var headers: [String: String] { defaultHeaders }
+    
+    var defaultHeaders: [String: String] {
+        ["Content-Type": "application/json"]
+    }
+}
+
+// MARK: - APIGetRequest
+
+protocol APIGetRequest: APIRequest {
+    var queryItems: [URLQueryItem] { get }
+}
+
+extension APIGetRequest {
+    var endpoint: URL {
+        instanceURL
+            .appending(path: path)
+            .appending(queryItems: queryItems.filter { $0.value != nil })
+    }
+}
+
+// MARK: - APIRequestBodyProviding
+
+protocol APIRequestBodyProviding: APIRequest {
+    associatedtype Body: Encodable
+    var body: Body { get }
+}
+
+extension APIRequestBodyProviding {
+    var endpoint: URL {
+        instanceURL
+            .appending(path: path)
+    }
+}
+
+// MARK: - APIPostRequest
+
+protocol APIPostRequest: APIRequestBodyProviding {}
+
+// MARK: - APIPutRequest
+
+protocol APIPutRequest: APIRequestBodyProviding {}
