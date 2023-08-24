cf4c5911c9064fa53a7f690bf82521c7e8e4cecc
Bosco Ho <boscojwho@gmail.com>
2023-08-24 02:18:05 -0700

    Initial commit for Xcode project.

---
+++ Cosmos/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Cosmos
+//
+//  Created by Bosco Ho on 2023-08-24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Cosmos/CosmosApp.swift
@@ -0,0 +1,17 @@
+//
+//  CosmosApp.swift
+//  Cosmos
+//
+//  Created by Bosco Ho on 2023-08-24.
+//
+
+import SwiftUI
+
+@main
+struct CosmosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
