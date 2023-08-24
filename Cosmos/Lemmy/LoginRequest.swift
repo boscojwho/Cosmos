97764407d48e0d8a4b85089a533b978d8041309b
Bosco Ho <boscojwho@gmail.com>
2023-08-24 03:34:22 -0700

    Add login request logic.

---
+++ Cosmos/Lemmy/LoginRequest.swift
@@ -0,0 +1,43 @@
+//
+//  LoginRequest.swift
+//  Cosmos
+//
+//  Created by Bosco Ho on 2023-08-24.
+//
+
+//  Addapted from:
+//
+//  LoginRequest.swift
+//  Mlem
+//
+//  Created by Nicholas Lawson on 05/06/2023.
+//
+
+import Foundation
+
+struct LoginRequest: APIPostRequest {
+    
+    typealias Response = LoginResponse
+    
+    let instanceURL: URL
+    let path = "user/login"
+    let body: Body
+    
+    // lemmy_api_common::person::Login
+    struct Body: Encodable {
+        let username_or_email: String
+        let password: String
+        let totp_2fa_token: String?
+    }
+    
+    init(instanceURL: URL, username: String, password: String, totpToken: String?) {
+        self.instanceURL = instanceURL
+        self.body = .init(username_or_email: username, password: password, totp_2fa_token: totpToken)
+    }
+}
+
+struct LoginResponse: Decodable {
+    let jwt: String
+    let registrationCreated: Bool
+    let verifyEmailSent: Bool
+}
